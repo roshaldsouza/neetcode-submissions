7da1c86640317bd7bc211831a4ca1d0940e621e8
Roshal Dsouza <110396825+roshaldsouza@users.noreply.github.com>
2026-09-20 20:10:48 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hp = {}
+        for num in nums:
+            if num in hp:
+                return True
+            else:
+                hp[num] = 1
+        return False
